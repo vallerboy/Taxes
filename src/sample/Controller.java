7217472a4a725cd81e665cac685f7346dae93c7c
Oskar Polak <biuro@fivarto.pl>
2018-04-13 21:36:39 +0200

    asd

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,9 +5,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.ResourceBundle;
 
@@ -19,7 +22,13 @@ public class Controller implements Initializable {
     @FXML
     Label labelShowSentence;
 
-    String[] sentenceArray = {"Dzień jak codzień", "Uśmiech to podstawa", "Pieniądze szczęścia nie dają", "Swiat jest piękny"};
+    @FXML
+    Button buttonAddNewSentence;
+
+    @FXML
+    TextField textfieldNewSentence;
+
+    List<String> listOfSentence = Arrays.asList("Dzień jak codzień", "Uśmiech to podstawa", "Pieniądze szczęścia nie dają", "Swiat jest piękny");
     Random random = new Random();
 
 
@@ -28,8 +37,10 @@ public class Controller implements Initializable {
         buttonClickMe.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                labelShowSentence.setText(sentenceArray[random.nextInt(sentenceArray.length)]);
+                labelShowSentence.setText(listOfSentence.get(random.nextInt(listOfSentence.size())));
             }
         });
+
+        
     }
 }
